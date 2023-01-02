496f6693c747230e5596f659197aef753a2b63c0
Dhairya <rupaladhairya@gmail.com>
2023-01-02 17:32:32 +0530

    Update react docs

--- Important/field2.js
+++ Important/field2.js
@@ -1,10 +1,7 @@
-"use strict";
-function ok(id) {
-    if (id == 0)
-        return undefined;
-    return id.toString();
+let a = [1, 2, 3]
+avg = 0;
+for (let item in a) {
+    console.log(item);
+    avg += item
 }
-const val = ok(0);
-const aval = ok(1);
-console.log(val === null || val === void 0 ? void 0 : val.toLowerCase());
-console.log(aval === null || aval === void 0 ? void 0 : aval.toUpperCase());
+console.log(avg);
