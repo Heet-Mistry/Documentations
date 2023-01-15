98243a671b1e0f313bb285126fdd91258a2afb01
Dhairya Rupala <rupaladhairya@gmail.com>
2023-01-15 23:50:07 +0530

    some more notes in react

--- Important/field.js
+++ Important/field.js
@@ -1,16 +1,25 @@
-let userStr = '{"name":"Sammy","email":"sammy@example.com","plan":"Pro"}';
+// let userStr = '{"name":"Sammy","email":"sammy@example.com","plan":"Pro"}';
 
-let userObj = JSON.parse(userStr);
+// let userObj = JSON.parse(userStr);
 
-console.log(userObj);
+// console.log(userObj);
 
-userStr = '{"name":"Sammy","email":"sammy@example.com","plan":"Pro"}';
+// userStr = '{"name":"Sammy","email":"sammy@example.com","plan":"Pro"}';
 
-userObj = JSON.parse(userStr, (key, value) => {
-  if (typeof value === 'string') {
-    return value.toUpperCase();
-  }
-  return value;
-});
+// userObj = JSON.parse(userStr, (key, value) => {
+//   if (typeof value === 'string') {
+//     return value.toUpperCase();
+//   }
+//   return value;
+// });
 
-console.log(userObj);
+// console.log(userObj);
+
+let arr = [1, 2, 3, 4, 5]
+console.log(arr.slice());
+// console.log(arr.splice(1, 2, "dhairya"));
+// console.log(arr);
+
+let d = arr.slice()
+d[0] = "ahsd"
+console.log(arr);
